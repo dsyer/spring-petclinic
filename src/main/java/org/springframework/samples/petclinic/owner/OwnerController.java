67ff881b18ce37dd44e616979772d59ba564a906
Dave Syer <dsyer@vmware.com>
2021-11-22 07:47:11 +0000

    Just use hx-boost on the table pagination
    It's a different approach and it shows how fragments can be used
    to render part of the page (not the whole body).

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -71,8 +71,7 @@ class OwnerController {
 	public String processCreationForm(@Valid Owner owner, BindingResult result) {
 		if (result.hasErrors()) {
 			return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
-		}
-		else {
+		} else {
 			this.owners.save(owner);
 			return "redirect:/owners/" + owner.getId();
 		}
@@ -100,17 +99,25 @@ class OwnerController {
 			// no owners found
 			result.rejectValue("lastName", "notFound", "not found");
 			return "owners/findOwners";
-		}
-		else if (ownersResults.getTotalElements() == 1) {
+		} else if (ownersResults.getTotalElements() == 1) {
 			// 1 owner found
 			owner = ownersResults.iterator().next();
 			return "redirect:/owners/" + owner.getId();
-		}
-		else {
+		} else {
 			// multiple owners found
 			lastName = owner.getLastName();
 			return addPaginationModel(page, model, lastName, ownersResults);
 		}
+	}
+
+	@GetMapping(path = "/owners", headers = "HX-Request=true")
+	public String processFindFormFragments(@RequestParam(defaultValue = "1") int page, Owner owner,
+			BindingResult result, Model model) {
+		String view = processFindForm(page, owner, result, model);
+		if (view.equals("owners/ownersList")) {
+			view = view + " :: list";
+		}
+		return view;
 	}
 
 	private String addPaginationModel(int page, Model model, String lastName, Page<Owner> paginated) {
@@ -143,8 +150,7 @@ class OwnerController {
 			@PathVariable("ownerId") int ownerId) {
 		if (result.hasErrors()) {
 			return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
-		}
-		else {
+		} else {
 			owner.setId(ownerId);
 			this.owners.save(owner);
 			return "redirect:/owners/{ownerId}";
@@ -153,6 +159,7 @@ class OwnerController {
 
 	/**
 	 * Custom handler for displaying an owner.
+	 *
 	 * @param ownerId the ID of the owner to display
 	 * @return a ModelMap with the model attributes for the view
 	 */
